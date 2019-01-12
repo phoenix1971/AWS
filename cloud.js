a7668160434270a82f0329c771580d6ad6b5d75c
Saad Muhammed Junayed <sm.junayed71@gmail.com>
2019-01-13 00:20:19 +0600

    Add files via upload

---
+++ cloud.js
@@ -0,0 +1,114 @@
+var udp = require('dgram');
+var buffer = require('buffer');
+
+// creating a cloud socket
+var cloud = udp.createSocket('udp4');
+
+function retrieve_flightNo(buffer)
+{
+	var buff = buffer.slice(0,1);
+	return buff;
+}
+var fs = require("fs");
+
+function writeToFile(msg)
+{
+
+   console.log("Going to write into file");
+   console.log("Asynchronous read: " );
+   console.log(msg);
+   fs.writeFile('input.txt',msg, function(err) {
+     if (err) {
+        return console.error(err);
+     }
+     console.log("Data written successfully!");
+   });
+   console.log("Let's read newly written data");
+   fs.readFile('input.txt', function (err, data) {
+      if (err) {
+         return console.error(err);
+      }
+      console.log("Asynchronous read: " );
+      console.log(data);
+      return data ;
+   });
+}
+function readToFile(){
+
+
+}
+//buffer msg
+var data = Buffer.from('...');
+var flight4 = Buffer.from([4]);
+var flight5 = Buffer.from([5]);
+var certificate = Buffer.from('...');
+cloud.on('message',function(msg,info){
+  var flight = retrieve_flightNo(msg) ;
+  console.log('Data received from server : ');
+  console.log(flight);
+  console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
+
+  if(flight.equals(flight4)){
+      certificate=msg ;
+      writeToFile(msg);
+      console.log("certificate stored in cloud");
+  }
+  if(flight.equals(flight5)){
+    var buffer_flight_data = new Buffer([7]);
+    console.log("Let's read newly written data");
+    fs.readFile('input.txt', function (err, data) {
+         if (err) {
+            return console.error(err);
+         }
+         console.log("Asynchronous read: " );
+         console.log(data);
+
+        certificate= Buffer.concat([buffer_flight_data,data]);
+        cloud.send(certificate,info.port,'13.233.166.81',function(error){
+          if(error){
+            console.log('!Data sent !!!');
+            console.log('certificate');
+            server.close();
+          }else{
+            console.log('Data sent !!!');
+            console.log('certificate');
+          }
+        });
+    });
+  }
+});
+
+//emits when socket is ready and listening for datagram msgs
+cloud.on('listening',function(){
+  var address = cloud.address();
+  var port = address.port;
+  var family = address.family;
+  var ipaddr = address.address;
+  console.log('cloud is listening at port' + port);
+  console.log('cloud ip :' + ipaddr);
+  console.log('cloud is IP4/IP6 : ' + family);
+});
+
+
+//sending msg
+/*cloud.send(data,2222,'localhost',function(error){
+  if(error){
+    cloud.close();
+  }else{
+    console.log('Data sent !!!');
+  }
+});
+
+var data1 = Buffer.from('hello');
+var data2 = Buffer.from('world');
+
+//sending multiple msg
+cloud.send([data1,data2],2222,'localhost',function(error){
+  if(error){
+    cloud.close();
+  }else{
+    console.log('Data sent !!!');
+  }
+});*/
+
+cloud.bind(2222);
---
+++ server2.js
@@ -0,0 +1,200 @@
+var udp = require('dgram');
+
+// --------------------creating a udp server --------------------
+
+// creating a udp server
+var server = udp.createSocket('udp4');
+
+//buffer msg
+var data = Buffer.from('siddheshrane2');
+
+
+// emits when any error occurs
+server.on('error',function(error){
+  console.log('Error: ' + error);
+  server.close();
+});
+
+
+
+function createHelloVerifyRequest()
+{
+	var buffer = new Buffer(258);
+
+	var buffer_flight_data = new Buffer([2]);
+	var buffer1_protocol_version = new Buffer([254,253]);
+	var buffer2_legacy_cookie = new Buffer(256);
+
+	buffer = Buffer.concat([buffer_flight_data, buffer1_protocol_version, buffer2_legacy_cookie]);
+
+	return buffer;
+}
+
+
+
+function createFlight4()
+{
+   var buffer = new Buffer(17);
+
+   var buffer_flight_data = new Buffer([4]);
+   var buffer1_server_hello = new Buffer(1);
+   var buffer2_server_certificate = new Buffer(1);
+   var buffer3_server_key_exchange = new Buffer(1);
+   var buffer4_client_certificate_request = new Buffer(1);
+   var buffer5_hello_done = new Buffer([1]);
+
+
+   buffer = Buffer.concat([buffer_flight_data, buffer1_server_hello, buffer2_server_certificate, buffer3_server_key_exchange, buffer4_client_certificate_request, buffer5_hello_done]);
+
+   return buffer;
+}
+
+/*
+clienthello
+client certificate
+client key exchange
+certificate verify
+change cipher specifications
+finished
+
+
+change cipher specifications
+finished
+
+
+*/
+
+
+
+
+function createFlight6()
+{
+   var buffer = new Buffer(2);
+
+   var buffer_flight_data = new Buffer([6]);
+   var buffer1_cipher_spec = new Buffer([1]);
+   var buffer2_finished = new Buffer([1]);
+
+
+   buffer = Buffer.concat([buffer_flight_data, buffer1_cipher_spec, buffer2_finished]);
+
+   return buffer;
+}
+
+
+
+function retrieve_flightNo(buffer)
+{
+	var buff = buffer.slice(0,1);
+	return buff;
+}
+
+//buffer msg
+var data  = Buffer.from('nothing');
+var flight1 = Buffer.from([1]);
+var flight2 = Buffer.from([2]);
+var flight3 = Buffer.from([3]);
+var flight4 = Buffer.from([4]);
+var flight5 = Buffer.from([5]);
+var flight6 = Buffer.from([6]);
+var cloud = Buffer.from([7]);
+var clientPort ;
+// emits on new datagram msg
+server.on('message',function(msg,info){
+  var flight = retrieve_flightNo(msg) ;
+  console.log(' Data received from client : ');
+  console.log(flight);
+  console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
+//sending msg
+  if(flight.equals(flight1)){
+    data=createHelloVerifyRequest();
+    server.send(data,info.port,info.address,function(error){
+      if(error){
+        console.log('!Data sent !!!');
+        console.log(retrieve_flightNo(data));
+        server.close();
+      }else{
+        console.log('Data sent !!!');
+        console.log(retrieve_flightNo(data));
+      }
+    });
+  }
+  if(flight.equals(flight3)){
+    console.log('flight 3.1');
+    data=createFlight4();
+    server.send(data,info.port,info.address,function(error){
+      if(error){
+        console.log('!Data sent !!!');
+        console.log(retrieve_flightNo(data));
+        server.close();
+      }else{
+        console.log('Data sent to client!!!');
+        console.log(retrieve_flightNo(data));
+      }
+    });
+    console.log('flight 3.2');
+    server.send(data,2222,info.address,function(error){
+      if(error){
+        console.log('!Data sent !!!');
+        console.log(retrieve_flightNo(data));
+        server.close();
+      }else{
+        console.log('Data sent to cloud!!!');
+        console.log(retrieve_flightNo(data));
+      }
+    });
+  }
+  if(flight.equals(flight5)){
+    console.log('flight 5');
+    data=flight5;
+    clientPort=info.port;
+    clientAddress=info.address;
+    console.log('info addres : '+info.address);
+    server.send(data,2222,'103.94.135.134',function(error){
+      if(error){
+        console.log('!Data sent !!!');
+        console.log(data);
+        server.close();
+      }else{
+        console.log('Data sent !!!');
+        console.log(data);
+      }
+    });
+  }
+  if(flight.equals(cloud)){
+    data=createFlight6();
+    server.send(data,clientPort,clientAddress,function(error){
+      if(error){
+        console.log('!Data sent !!!');
+        console.log(retrieve_flightNo(data));
+        server.close();
+      }else{
+        console.log('Data sent !!!');
+        console.log(retrieve_flightNo(data));
+      }
+    });
+  }
+
+});
+
+//emits when socket is ready and listening for datagram msgs
+server.on('listening',function(){
+  var address = server.address();
+  var port = address.port;
+  var family = address.family;
+  var ipaddr = address.address;
+  console.log('Server is listening at port' + port);
+  console.log('Server ip :' + ipaddr);
+  console.log('Server is IP4/IP6 : ' + family);
+});
+
+//emits after the socket is closed using socket.close();
+server.on('close',function(){
+  console.log('Socket is closed !');
+});
+
+server.bind(2222);
+
+setTimeout(function(){
+  //server.close();
+},8000);
